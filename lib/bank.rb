46e7d2ff0f7ed2b870fded95c3c3a330ec1f8f30
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-03-12 13:42:21 +0000

    added timestamps to transactions

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,16 +1,19 @@
 class Bank
-    attr_reader :balance
+    attr_reader :balance, :statement
 
     def initialize
         @balance = 0
+        @statement = {}
     end
 
-    def deposit(amount)
+    def deposit(date, amount)
         @balance += amount
+        @statement[date] = amount
     end
 
-    def withdraw(amount)
+    def withdraw(date, amount)
         @balance -= amount
+        @statement[date] = -amount
     end
 
 
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe Bank do
     bank = Bank.new
     context "#deposit" do
         it 'deposit adds to balance by deposited amount' do      
-            expect{bank.deposit(1000)}.to change {bank.balance}.by(1000)
+            expect{bank.deposit("10/01/2012", 1000)}.to change {bank.balance}.by(1000)
         end   
     end 
     context "#withdraw" do
         it "withdraw reduces balance by amount withdrawn" do
-            expect{bank.withdraw(50)}.to change {bank.balance}.by(-50)
+            expect{bank.withdraw("14/01/2012", 50)}.to change {bank.balance}.by(-50)
+        end
+    end
+
+    context 'date' do
+        it "transactions carry a date stamp" do
+            bank.withdraw("14/10/2019", 50)
+            bank.deposit("13/01/2012", 2000)
+            expect(bank.statement).to include("14/10/2019" => -50)
+            expect(bank.statement).to include("13/01/2012" => 2000)
         end
     end
 end
