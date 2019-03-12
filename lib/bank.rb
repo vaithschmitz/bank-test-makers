f0bb39cf7e897aa906e0f87a669d1854d5ae81a8
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-03-12 15:18:59 +0000

    added reversed output

--- lib/bank.rb
+++ lib/bank.rb
@@ -8,13 +8,24 @@ class Bank
 
     def deposit(date, amount)
         @balance += amount
-        @statement[date] = amount
+        @statement[date] = amount, balance
     end
 
     def withdraw(date, amount)
         @balance -= amount
-        @statement[date] = -amount
+        @statement[date] = -amount, balance
+    end
+
+    def print_statement
+        rev = @statement.to_a.reverse.to_h
+        rev.each{|key, val| puts "#{key}, #{val[0]}, #{val[1]}"}
     end
 
 
 end
+
+bank = Bank.new
+bank.deposit('14/10/2012', 500)
+bank.deposit('15/10/2012', 600)
+bank.withdraw('17/10/2012', 520)
+bank.print_statement
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,24 +1,35 @@
 require 'bank'
 
 RSpec.describe Bank do
-    bank = Bank.new
+    before(:each) do 
+    @bank = Bank.new
+    end
     context "#deposit" do
         it 'deposit adds to balance by deposited amount' do      
-            expect{bank.deposit("10/01/2012", 1000)}.to change {bank.balance}.by(1000)
+            expect{@bank.deposit("10/01/2012", 1000)}.to change {@bank.balance}.by(1000)
         end   
     end 
     context "#withdraw" do
         it "withdraw reduces balance by amount withdrawn" do
-            expect{bank.withdraw("14/01/2012", 50)}.to change {bank.balance}.by(-50)
+            expect{@bank.withdraw("14/01/2012", 50)}.to change {@bank.balance}.by(-50)
         end
     end
 
     context 'date' do
         it "transactions carry a date stamp" do
-            bank.withdraw("14/10/2019", 50)
-            bank.deposit("13/01/2012", 2000)
-            expect(bank.statement).to include("14/10/2019" => -50)
-            expect(bank.statement).to include("13/01/2012" => 2000)
+            @bank.deposit("13/01/2012", 2000)
+            @bank.withdraw("14/10/2019", 50)
+            expect(@bank.statement).to include("13/01/2012" => [2000, 2000])
+            expect(@bank.statement).to include("14/10/2019" => [-50, 1950])
+        end
+    end
+
+    context '#print_statement' do
+        it "prints a list of transactions" do
+            @bank.deposit("13/01/2012", 2000)
+            @bank.withdraw("14/10/2019", 50)
+            @bank.withdraw("15/10/2019", 500)
+            expect{@bank.print_statement}.to output("15/10/2019, -500, 1450\n14/10/2019, -50, 1950\n13/01/2012, 2000, 2000\n").to_stdout
         end
     end
 end
